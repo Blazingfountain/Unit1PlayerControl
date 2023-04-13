c1536a1750d3627e394c14c903b279f6181df4b9
Chase <chase.hyska@gmail.com>
2023-04-13 13:53:27 -0400

    unit 1.4

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,19 @@ public class PlayerController : MonoBehaviour
     {
        
     }
-
+    private float speed = 15.0f;
+    private float turnSpeed = 40.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Update is called once per frame
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // moves the car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // rotates the car
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
     }
 }
